839d0aff9f8bdd136424a1e6727dbf5c6207554c
JD Bothma <jbothma@gmail.com>
2019-09-24 22:02:13 +0200

    Schema from model

--- connector.js
+++ connector.js
@@ -1,45 +1,51 @@
 (function () {
+    var dataset_url = "https://openspending.org/api/3/cubes/b9d2af843f3a7ca223eea07fb608e62a:budgeted-and-actual-national-expenditure-uploaded-2019-04-16t1104";
     var myConnector = tableau.makeConnector();
-
+    var get_ref = function(model, dimension_name, ref_type) {
+        return model.dimensions[dimension_name][ref_type + "_ref"];
+    };
+    var get_dimension = function(model, hierarchy_name, level) {
+        level = typeof level !== 'undefined' ? level : 0;
+        return model.hierarchies["hierarchy_name"].levels["level"];
+    };
+    var openspendingToTableauType = {
+        string: tableau.dataTypeEnum.string,
+        integer: tableau.dataTypeEnum.int,
+    };
     myConnector.getSchema = function (schemaCallback) {
-        var cols = [
-            {
-                id: "phase",
-                alias: "Budget Phase",
-                dataType: tableau.dataTypeEnum.string
-            },
-            {
-                id: "financial_year",
-                alias: "Financial Year",
-                dataType: tableau.dataTypeEnum.int
-            },
-            {
-                id: "programme",
-                alias: "Programme",
-                dataType: tableau.dataTypeEnum.string
-            },
-            {
-                id: "economic_classification",
-                alias: "Economic Classification",
-                dataType: tableau.dataTypeEnum.string
-            },
-            {
-                id: "amount",
-                dataType: tableau.dataTypeEnum.float
-            }
-        ];
+        var model_url = dataset_url + "/model/";
+        $.getJSON(model_url, function(response) {
+            var model = response.model;
+            var cols = [];
+            for (var hierarchyId in model.hierarchies) {
+                var hierarchy = model.hierarchies[hierarchyId];
+                for (var index = 0; index < hierarchy.levels.length; index++) {
+                    var columnId;
+                    if (index == 0)
+                        columnId = hierarchyId;
+                    else
+                        columnId = hierarchyId + "_" + (index + 1);
+                    var dimensionId = hierarchy.levels[index];
+                    var dimension = model.dimensions[dimensionId];
+                    cols.push({
+                        id: columnId,
+                        dataType: openspendingToTableauType[dimension.attributes[dimensionId].type]
+                    });
+                }
+            };
 
-        var tableSchema = {
-            id: "spending",
-            alias: "Spending Data",
-            columns: cols
-        };
+            var tableSchema = {
+                id: "spending",
+                alias: "Spending Data",
+                columns: cols
+            };
 
-        schemaCallback([tableSchema]);
+            schemaCallback([tableSchema]);
+        });
     };
 
     myConnector.getData = function (table, doneCallback) {
-        var facts_url = "https://openspending.org/api/3/cubes/b9d2af843f3a7ca223eea07fb608e62a:budgeted-and-actual-national-expenditure-uploaded-2019-04-16t1104/facts/";
+        var facts_url = dataset_url + "/facts/";
         $.getJSON(facts_url, function(resp) {
             var tableData = resp.data.map(function(row) {
                 return {
